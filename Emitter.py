effae91d5c0ee9203950c5e42e930054c6869cc3
Ian Lasky <ian.lasky1@gmail.com>
2019-02-05 15:20:47 -0500

    Added config upload capability

--- Emitter.py
+++ Emitter.py
@@ -1,11 +1,15 @@
 import os
 import time
+import json
 import pickle
 import numpy as np
 import pandas as pd
+import pyperclip as pc
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui  import Select
+from selenium.common.exceptions import InvalidElementStateException
 
 class Emitter(object):
     """Emitter object capable of automatically running ESPET website"""
@@ -80,6 +84,8 @@ class Emitter(object):
         """Connect to the ESPET quicksolver"""
 
         chrome_options = webdriver.ChromeOptions()
+        if not os.path.exists('output_data'):
+            os.makedirs('output_data')
         prefs = {'download.default_directory' : os.getcwd() + '/output_data/'}
         chrome_options.add_experimental_option('prefs', prefs)
 
@@ -97,18 +103,17 @@ class Emitter(object):
     def select_props(self):
         """Select the emitter, feed, and independent variable"""
 
-        emitter_select = Select(self.driver.find_element_by_name('emitter'))
-        emitter_select.select_by_value(self._emitter)
-
-        feed_select = Select(self.driver.find_element_by_name('feed'))
-        feed_select.select_by_value(self._feed)
-
-        independent_variable_select = Select(self.driver.find_element_by_name('independentVariable'))
-        independent_variable_select.select_by_value(self._independent_variable)
-
         try:
             self.__get_fields()
         except:
+            emitter_select = Select(self.driver.find_element_by_name('emitter'))
+            emitter_select.select_by_value(self._emitter)
+
+            feed_select = Select(self.driver.find_element_by_name('feed'))
+            feed_select.select_by_value(self._feed)
+
+            independent_variable_select = Select(self.driver.find_element_by_name('independentVariable'))
+            independent_variable_select.select_by_value(self._independent_variable)
             self.scrape_fields()
             self.save_fields()
 
@@ -124,8 +129,17 @@ class Emitter(object):
                     '.pickle')
             with open(fname, 'rb') as f:
                 self._input_fields = pickle.load(f)
+
+            self._input_fields['emitter'] = self.emitter
+            self._input_fields['feed'] = self.feed
+            self._input_fields['propellant'] = self.propellant
+            self._input_fields['substrate_emitter'] = self.emitter_substrate
+            self._input_fields['substrate_feed'] = self.feed_substrate
+            self._input_fields['independentVariable'] = self.independent_variable
+            self._input_fields['field'] = self.field
+
         except FileNotFoundError:
-            pass
+            raise FileNotFoundError('Could not find input fields file.')
 
 
     def scrape_fields(self):
@@ -146,10 +160,8 @@ class Emitter(object):
                 feed_substrate_options if not o.get_attribute('disabled')]
 
         input_fields = self.driver.find_elements_by_xpath("//input[@type='text']")
-        field_names = [str(i.get_attribute('name')) for i in input_fields if not
-                i.get_attribute('readonly')]
-        field_vals = [str(i.get_attribute('value')) for i in input_fields if not
-                i.get_attribute('readonly')]
+        field_names = [str(i.get_attribute('name')) for i in input_fields]
+        field_vals = [str(i.get_attribute('value')) for i in input_fields]
 
         self._input_fields = dict(zip(field_names, field_vals))
 
@@ -174,17 +186,37 @@ class Emitter(object):
     def upload_data_individual(self):
         """Upload the data field by field"""
 
+        ind_var = self.driver.find_element_by_name('independentVariable')
         for key, val in self._input_fields.items():
             temp = self.driver.find_element_by_name(key)
             if key not in ['emitter', 'feed', 'propellant', 'substrate_emitter',
-                    'substrate_feed', 'field']:
+                    'substrate_feed', 'field', 'independentVariable']:
+                i = 0
+                if temp.get_attribute('readonly'):
+                    while temp.get_attribute('readonly'):
+                        Select(ind_var).select_by_index(i % 3)
+                        i  = (i + 1) % 3
                 temp.clear()
                 temp.send_keys(val)
+
             else:
                 Select(temp).select_by_value(val)
 
+        Select(ind_var).select_by_value(self.independent_variable)
+
     def upload_data_group(self):
         """Upload data using the upload config function on ESPET"""
+
+        upload_button = self.driver.find_element_by_xpath('//*[@id="mainForm"]/table/tbody/tr/td[2]/table/tbody/tr/td[2]/table/tbody/tr[3]/td/input')
+        upload_button.click()
+
+        config_area = self.driver.find_element_by_name('Config')
+        config_area.click()
+        s = json.dumps(self._input_fields)
+        pc.copy(s)
+        config_area.send_keys(Keys.CONTROL, 'v')
+        load_button = self.driver.find_element_by_name('Load Config')
+        load_button.click()
 
 
     def run_sim(self):
@@ -196,12 +228,13 @@ class Emitter(object):
         """Save the simulation data and create pandas object"""
 
         self.driver.find_element_by_name('Save Data').click()
+        time.sleep(1)
 
         s = 'output_data/' + self._emitter + ' and ' + self._feed + '.csv'
 
         self.sim_data= pd.read_csv(s)
-        time.sleep(0.05)
-        os.remove(s)
+        #time.sleep(0.05)
+        #os.remove(s)
 
     @property
     def emitter(self):
@@ -209,7 +242,7 @@ class Emitter(object):
 
     @emitter.setter
     def emitter(self, value):
-        if np.issubdtype(type(value), np.string_) and value in self.__emitters:
+        if np.issubdtype(type(value), np.str_) and value in self.__emitters:
             self._emitter = value
         elif np.issubdtype(type(value), np.int64) and value < len(self.__emitters):
             self._emitter = self.__emitters[value]
@@ -222,7 +255,7 @@ class Emitter(object):
 
     @feed.setter
     def feed(self, value):
-        if np.issubdtype(type(value), np.string_) and value in self.__feeds:
+        if np.issubdtype(type(value), np.str_) and value in self.__feeds:
             self._feed= value
         elif np.issubdtype(type(value), np.int64) and value < len(self.__feeds):
             self._feed= self.__feeds[value]
@@ -235,7 +268,7 @@ class Emitter(object):
 
     @propellant.setter
     def propellant(self, value):
-        if np.issubdtype(type(value), np.string_) and value in self.__propellants:
+        if np.issubdtype(type(value), np.str_) and value in self.__propellants:
             self._propellant = value
         elif np.issubdtype(type(value), np.int64) and value < len(self.__propellants):
             self._propellant = self.__propellants[value]
@@ -248,7 +281,7 @@ class Emitter(object):
 
     @independent_variable.setter
     def independent_variable(self, value):
-        if np.issubdtype(type(value), np.string_) and value in self.__independent_variables:
+        if np.issubdtype(type(value), np.str_) and value in self.__independent_variables:
             self._independent_variable = value
         elif np.issubdtype(type(value), np.int64) and value < len(self.__independent_variables):
             self._independent_variable = self.__independent_variabls[value]
@@ -261,7 +294,7 @@ class Emitter(object):
 
     @emitter_substrate.setter
     def emitter_substrate(self, value):
-        if np.issubdtype(type(value), np.string_) and value in self.__emitter_substrates:
+        if np.issubdtype(type(value), np.str_) and value in self.__emitter_substrates:
             self._emitter_substrate = value
         elif np.issubdtype(type(value), np.int64) and value < len(self.__emitter_substrates):
             self._emitter_substrate = self.__emitter_substrates[value]
@@ -274,7 +307,7 @@ class Emitter(object):
 
     @feed_substrate.setter
     def feed_substrate(self, value):
-        if np.issubdtype(type(value), np.string_) and value in self.__feed_substrates:
+        if np.issubdtype(type(value), np.str_) and value in self.__feed_substrates:
             self._feed_substrate = value
         elif np.issubdtype(type(value), np.int64) and value < len(self.__feed_substrates):
             self._feed_substrate = self.__feed_substrates[value]
@@ -287,7 +320,7 @@ class Emitter(object):
 
     @field.setter
     def field(self, value):
-        if np.issubdtype(type(value), np.string_) and value in self.__fields:
+        if np.issubdtype(type(value), np.str_) and value in self.__fields:
             self._field = value
         elif np.issubdtype(type(value), np.int64) and value < len(self.__fields):
             self._field= self.__fields[value]
